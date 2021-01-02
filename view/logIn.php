7af8ce21c6fed039ff30aa2cfc7c6a157e18b708
Ashraf236 <you@example.com>
2021-01-02 11:37:10 +0600

    Login And Registration validation with js and php complete

---
+++ view/logIn.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log In</title>
+</head>
+<body>
+	<form method="POST" action="../php/logInCheck.php" onsubmit="return validate()">
+		<fieldset>
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" id="username" name="username" onkeyup=" checkUser()" onkeypress="nameRemover()" onblur="blr()"/></td>
+					<td id="userMsg"> </td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" id="password" name="password" onkeyup="passRemover()"/></td>
+					<td id="passMsg"> </td>
+					<td></td>
+				</tr>
+				<tr>
+					<td ></td>
+					<td><input type="submit" name="submit" value="submit" onmousemove=" validate()"/>
+					
+                      <a href="../view/registration.php">Register</a><br>
+                      <a href="../">Back</a>
+					</td>
+					<td id="errorMsg"></td>
+				</tr>
+
+			</table>
+		 </fieldset>
+	</form>
+	<script type="text/javascript" src="../script/script.js"></script>
+</body>
+</html>
+
+<?php
+ 
+	
+	if(isset($_GET['null_sub'])){
+
+	 echo"All input field must be inserted";
+	}
+	elseif(isset($_GET['invalid_detail'])){
+		
+	  echo"No user found. wrong Username/ password";
+	}
+
+?>
+
